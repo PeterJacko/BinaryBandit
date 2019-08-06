d0824217d314615f38d0b61881da4e47de14e4f0
PeterJacko <peter.jacko@gmail.com>
2019-08-06 22:08:38 +0100

    creating tests
    creating tests

--- src/BinaryBandit.jl
+++ src/BinaryBandit.jl
@@ -5,7 +5,6 @@ module BinaryBandit
 const BB_numerical_precision_64 = 1.0e-13 # instead of 16
 const BB_numerical_precision_32 = 1.0e-4 # instead of 7
 
-include( "BB_misc.jl" )
 include( "BB_DP_2.jl" )
 include( "BB_evaluation_DP_2.jl" )
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,39 @@
 using BinaryBandit
 
-BinaryBandit.DP_2_policy_lin( 35 )
+horizon = 60
+
+BinaryBandit.DP_2_action_lin( horizon , 64 )
+BinaryBandit.DP_2_action_lin( horizon , 32 )
+BinaryBandit.DP_2_action_lin( horizon , 16 )
+
+BinaryBandit.DP_2_action_lin_with_administration( horizon , 0 , 64 )
+BinaryBandit.DP_2_action_lin_with_administration( horizon , 0 , 32 )
+BinaryBandit.DP_2_action_lin_with_administration( horizon , 0 , 16 )
+
+BinaryBandit.DP_2_action( horizon , 64 )
+BinaryBandit.DP_2_action( horizon , 32 )
+BinaryBandit.DP_2_action( horizon , 16 )
+
+BinaryBandit.DP_2_action_with_administration( horizon , 0 , 64 )
+BinaryBandit.DP_2_action_with_administration( horizon , 0 , 32 )
+BinaryBandit.DP_2_action_with_administration( horizon , 0 , 16 )
+
+BinaryBandit.DP_2_policy_bin_lin( horizon )
+BinaryBandit.DP_2_policy_bin_lin_with_administration( horizon , 0 )
+
+BinaryBandit.DP_2_policy_bin( horizon )
+BinaryBandit.DP_2_policy_bin_with_administration( horizon , 0 )
+
+BinaryBandit.DP_2_policy_lin_new( horizon )
+
+BinaryBandit.DP_2_policy_lin( horizon )
+BinaryBandit.DP_2_policy_lin_with_administration( horizon , 0 )
+
+BinaryBandit.DP_2_policy( horizon )
+BinaryBandit.DP_2_policy_with_administration( horizon , 0 )
+
+
+success_probability_arm_1 = 0.3
+success_probability_arm_2 = 0.5
+
+BinaryBandit.DP_2_NS( horizon , success_probability_arm_1 , success_probability_arm_2 )
